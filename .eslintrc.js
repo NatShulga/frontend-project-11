8824afbef2e7145bdb903ae538a5bfac8ee2c684
online shop <dove.sh@yandex.ru>
2025-02-28 19:49:40 +0300

    добавление конфига для линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     'spellcheck/spell-checker': 'off',
+    'function-paren-newline': ['error', 'multiline'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: ['error', 'always'],
