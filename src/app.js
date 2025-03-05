1bec7f1f200827b27f54b2fbf20a448b3ebf2a56
online shop <dove.sh@yandex.ru>
2025-03-05 08:31:49 +0300

    правила линтера

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable indent */
+/* eslint-disable indent */ /*eslint space-in-parens: ["error", "never"]*/ 
 import * as yup from 'yup';
 import i18next from 'i18next';
 import axios from 'axios';
